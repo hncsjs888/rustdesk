933964a19d0cec1579aca8335e1b18ba42d3827d
hncsjs888 <hncsjs888@gmail.com>
2025-01-10 22:35:46 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.7803k.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "0xmKQrbWWyE6b4Yaa5ywK9sKgDqEV3V8f5wf8MssUQc=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["91rui.top"];
+pub const PUBLIC_RS_PUB_KEY: &str = "18008441958";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
